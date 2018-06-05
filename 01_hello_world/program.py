d2c90059cd8c50ca605efad55015735ce2d6cf2c
Unknown <anthonyantho16@hotmail.com>
2018-06-04 23:40:16 -0400

    lesson 02 initial commit

--- 01_hello_world/program.py
+++ 01_hello_world/program.py
@@ -2,7 +2,6 @@ print('-------------------------')
 print('       HELLO WORLD!      ')
 print('-------------------------')
 
-
 user_text = input('What is your name? ')
 greeting = 'Nice to meet you ' + user_text
 print (greeting)
